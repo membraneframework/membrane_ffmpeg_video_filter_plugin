e9067b9ec4137f437f9c8eae0c62227ddbeb5ccc
Agnieszka Dutka <agnieszka.dutka@swmansion.com>
2021-12-17 11:21:39 +0100

    add text_stream option for dynamic text

--- lib/membrane_ffmpeg_video_filter/text_overlay.ex
+++ lib/membrane_ffmpeg_video_filter/text_overlay.ex
@@ -8,13 +8,25 @@ defmodule Membrane.FFmpeg.VideoFilter.TextOverlay do
   Additionally, the element has to receive proper caps with picture format and dimensions.
   """
   use Membrane.Filter
+
+  require Membrane.Logger
+
   alias __MODULE__.Native
-  alias Membrane.Buffer
   alias Membrane.Caps.Video.Raw
 
   def_options text: [
                 type: :binary,
-                description: "Text to be displayed on video"
+                description:
+                  "Text to be displayed on video. Either text or text_stream must be provided",
+                default: nil
+              ],
+              text_stream: [
+                type: :list,
+                spec: [{{Time.t(), Time.t() | :infinity}, binary()}],
+                description:
+                  "List of time intervals when each given text should appear. Intervals should not overlap.
+                Either text or text_stream must be provided",
+                default: []
               ],
               fontsize: [
                 type: :int,
@@ -70,12 +82,33 @@ defmodule Membrane.FFmpeg.VideoFilter.TextOverlay do
 
   @impl true
   def handle_init(options) do
+    text_stream = convert_to_text_stream(options)
+
     state =
       options
       |> Map.from_struct()
+      |> Map.delete(:text)
+      |> Map.put(:text_stream, text_stream)
       |> Map.put(:native_state, nil)
 
     {:ok, state}
+  end
+
+  defp convert_to_text_stream(%{text: nil, text_stream: []}) do
+    Membrane.Logger.warn("No text or text_stream provided, no text will be added to video")
+    []
+  end
+
+  defp convert_to_text_stream(%{text: nil, text_stream: text_stream}) do
+    text_stream
+  end
+
+  defp convert_to_text_stream(%{text: text, text_stream: []}) do
+    [{{0, :infinity}, text}]
+  end
+
+  defp convert_to_text_stream(%{text: _text, text_stream: _text_stream}) do
+    raise("Both 'text' and 'text_stream' have been provided - choose one input method.")
   end
 
   @impl true
@@ -90,17 +123,57 @@ defmodule Membrane.FFmpeg.VideoFilter.TextOverlay do
   end
 
   @impl true
-  def handle_caps(
-        :input,
-        %Raw{format: format, width: width, height: height} = caps,
-        _context,
-        state
-      ) do
+  def handle_caps(:input, caps, _context, state) do
+    state = init_new_filter_if_needed(caps, state)
+    {{:ok, caps: {:output, caps}, redemand: :output}, state}
+  end
+
+  @impl true
+  def handle_process(:input, buffer, ctx, state) do
+    {buffer, state} = apply_filter_if_needed(buffer, ctx, state)
+    {{:ok, [buffer: {:output, buffer}]}, state}
+  end
+
+  # no text left to render
+  defp apply_filter_if_needed(buffer, _ctx, %{text_stream: []} = state) do
+    {buffer, state}
+  end
+
+  defp apply_filter_if_needed(
+         %{metadata: metadata, payload: payload} = buffer,
+         ctx,
+         %{native_state: native_state, text_stream: [{interval, _text} | streams]} = state
+       ) do
+    cond do
+      frame_before_interval?(metadata, interval) ->
+        {buffer, state}
+
+      frame_after_interval?(metadata, interval) ->
+        state = %{state | text_stream: streams}
+        state = init_new_filter_if_needed(ctx.pads.input.caps, state)
+        apply_filter_if_needed(buffer, ctx, state)
+
+      frame_in_interval?(metadata, interval) ->
+        case Native.apply_filter(payload, native_state) do
+          {:ok, frame} ->
+            {%{buffer | payload: frame}, state}
+
+          {:error, reason} ->
+            raise inspect(reason)
+        end
+    end
+  end
+
+  defp init_new_filter_if_needed(_caps, %{text_stream: []} = state), do: state
+
+  defp init_new_filter_if_needed(caps, %{text_stream: [stream | _streams]} = state) do
+    {_interval, text} = stream
+
     case Native.create(
-           state.text,
-           width,
-           height,
-           format,
+           text,
+           caps.width,
+           caps.height,
+           caps.format,
            state.fontsize,
            state.box?,
            state.boxcolor,
@@ -111,29 +184,37 @@ defmodule Membrane.FFmpeg.VideoFilter.TextOverlay do
            state.vertical_align
          ) do
       {:ok, native_state} ->
-        state = %{state | native_state: native_state}
-        {{:ok, caps: {:output, caps}, redemand: :output}, state}
+        %{state | native_state: native_state}
 
       {:error, reason} ->
         raise inspect(reason)
     end
   end
 
-  @impl true
-  def handle_process(
-        :input,
-        %Buffer{payload: payload} = buffer,
-        _ctx,
-        %{native_state: native_state} = state
-      ) do
-    case Native.apply_filter(payload, native_state) do
-      {:ok, frame} ->
-        buffer = [buffer: {:output, %{buffer | payload: frame}}]
-        {{:ok, buffer}, state}
+  defp frame_before_interval?(metadata, {from, _to}) do
+    pts = get_pts(metadata)
+    pts < from
+  end
 
-      {:error, reason} ->
-        raise inspect(reason)
-    end
+  defp frame_after_interval?(_metadata, {_from, :infinity}), do: false
+
+  defp frame_after_interval?(metadata, {_from, to}) do
+    pts = get_pts(metadata)
+    pts >= to
+  end
+
+  defp frame_in_interval?(metadata, {from, :infinity}) do
+    pts = get_pts(metadata)
+    pts >= from
+  end
+
+  defp frame_in_interval?(metadata, {from, to}) do
+    pts = get_pts(metadata)
+    pts < to and pts >= from
+  end
+
+  defp get_pts(metadata) do
+    max(Ratio.floor(metadata.pts), 0)
   end
 
   @impl true
