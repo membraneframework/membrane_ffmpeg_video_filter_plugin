5863eb28cbe388f0d008ca2a616dc9ac19300fcc
varsill <lukasz.kita0@gmail.com>
2022-05-04 09:52:26 +0200

    bump to 0.7.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.FFmpeg.VideoFilter.Mixfile do
   use Mix.Project
 
-  @version "0.6.1"
+  @version "0.7.0"
   @github_url "https://github.com/membraneframework/membrane_ffmpeg_video_filter_plugin"
 
   def project do
@@ -35,9 +35,9 @@ defmodule Membrane.FFmpeg.VideoFilter.Mixfile do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.9.0"},
+      {:membrane_core, "~> 0.10.0"},
       {:membrane_raw_video_format, "~> 0.2.0"},
-      {:membrane_common_c, "~> 0.11.0"},
+      {:membrane_common_c, "~> 0.12.0"},
       {:unifex, "~> 0.7.2"},
       # Testing
       {:membrane_file_plugin, "~> 0.9", only: :test},
