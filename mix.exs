a086135121785e152bb6c2e1dd22f7b62cec464f
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 11:26:24 +0200

    Set ex_doc dependency to >= 0.40.0

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule Membrane.FFmpeg.VideoFilter.Mixfile do
       {:membrane_h264_ffmpeg_plugin, "~> 0.32.0", only: :test},
       {:membrane_h264_plugin, "~> 0.9.0", only: :test},
       # Development
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.40.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false}
     ]
